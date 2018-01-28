087e36239f4a3d64a82e561a75b46d119a65282b
Petr Matukhov <petr.matukhov@gmail.com>
2018-01-28 17:39:14 +0300

    db settings

--- hasker/settings.py
+++ hasker/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'HOST':'127.0.0.1',
         'PORT':'3306',
         'USER': 'root',
-        'PASSWORD': 'root',
+        'PASSWORD': '',
         'NAME': 'hasker',
         'TEST':{
             'NAME':'hasker_test'
