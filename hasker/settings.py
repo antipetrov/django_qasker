164f3514e3ec67c54f9d4e887e31a304ac167c3c
Petr Matukhov <petr.matukhov@gmail.com>
2018-02-01 21:50:06 +0300

    add app

--- hasker/settings.py
+++ hasker/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'answers.apps.AnswersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
