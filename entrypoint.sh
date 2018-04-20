0e039f657431fbf9058d58ba0bb3006d7cc0484f
Petr Matukhov <petr.matukhov@gmail.com>
2018-04-20 21:26:35 +0300

    collect static - after start 2

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,6 @@ export DJANGO_SETTINGS_MODULE=hasker.settings_docker
 
 mysql -h mysql -u root -proot < init.sql
 
-python manage.py collectstatic
-
 python manage.py migrate
 
 uwsgi --ini /opt/hasker/uwsgi.ini
