1efc65e688c99b6a1a5b13b1e5c540c55f0b29db
Petr Matukhov <petr.matukhov@gmail.com>
2018-04-20 20:47:48 +0300

    key

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,9 @@
 # ssh hasker@37.139.2.116 -i .travis/key_hasker
 # 
 eval "$(ssh-agent -s)" # Start ssh-agent cache
+
+openssl aes-256-cbc -K $encrypted_65a296283a37_key -iv $encrypted_65a296283a37_iv -in key_hasker.enc -out key_hasker -d
+
 chmod 600 key_hasker # Allow read access to the private key
 ssh-add key_hasker # Add the private key to SSH
 
@@ -11,7 +14,8 @@ git config --global push.default matching
 git remote add deploy ssh://git@$IP:$PORT$DEPLOY_DIR
 git push deploy master
 
-# Skip this command if you don't need to execute any additional commands after deploying.
+ls
+
 ssh hasker@$IP -p $PORT -i key_hasker <<EOF
   cd $DEPLOY_DIR
   make build
